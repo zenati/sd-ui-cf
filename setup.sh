e4879e4051b7dc42cd6d9a45aba99eb3006132a3
Heiko Hotz <heiko.hotz@posteo.net>
2022-12-27 15:26:15 +0000

    One-click deployment

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+# disable the restart dialogue and install several packages
 sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 sudo apt-get update
 sudo apt install wget git python3 python3-venv build-essential net-tools -y
@@ -6,24 +7,26 @@ sudo apt install wget git python3 python3-venv build-essential net-tools -y
 wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda_12.0.0_525.60.13_linux.run
 sudo sh cuda_12.0.0_525.60.13_linux.run --silent
 
-# git clone https://github.com/marshmellow77/stable-diffusion-webui.git
-
 # install git-lfs
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
 sudo -u ubuntu git lfs install --skip-smudge
 
+# download the SD model v2.1 and move it to the SD model directory
 sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
 cd stable-diffusion-2-1-base/
 sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
 sudo -u ubuntu git lfs install --force
-
 cd ..
 mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
+
+# download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
 wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
 cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
-sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
-# cd stable-diffusion-webui/
 
+# change ownership of the web UI so that a regular user can start the server
+sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
+
+# start the server as user 'ubuntu'
 sudo -u ubuntu nohup bash stable-diffusion-webui/webui.sh --listen > log.txt
 
